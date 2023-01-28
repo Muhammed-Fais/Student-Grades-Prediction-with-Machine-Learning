f82597ff5b3670211f1216283060fbb8d851b144
Muhammed-Fais <fayismohd1123@gmail.com>
2023-01-28 14:24:26 +0400

    Streamlite O/P

--- App.py
+++ App.py
@@ -8,6 +8,7 @@ Created on Thu Jan 26 11:37:29 2023
 import streamlit as st
 import pandas as pd
 import pickle
+from PIL import Image
 
 # Load your trained model
 with open('linear_regression.pkl', 'rb') as f:
@@ -21,11 +22,27 @@ def predict(data:pd.DataFrame):
     return result
 
 st.title("Student Mark Predictor")
+html_temp = """
+<div style="background-color:tomato;padding:10px">
+<h2 style="color:white;text-align:center;">Streamlit Bank Note Authenticator ML App </h2>
+</div>
+"""
 
-file_upload = st.file_uploader("Upload a csv file", type=["csv"])
+Grade1 = st.number_input("Enter Grade1 out of 100")
+Grade2 = st.number_input("Enter Grade2 out of 100")
+Study_Time = st.number_input("Enter Study Time in hours per week")
+Failures = st.number_input("Enter the number of past class failures")
+Absence = st.number_input("Enter the number of school absence days")
 
-if file_upload is not None:
-    data = pd.read_csv(file_upload)
-    if st.button("Predict"):
-        result = predict(data)
-        st.success('The predicted marks are {}'.format(result))
+data = {'Grade1':[Grade1],'Grade2':[Grade2],'Study_Time':[Study_Time],'Failures':[Failures],'Absence':[Absence]}
+data = pd.DataFrame(data)
+
+if st.button("Predict"):
+    result = predict(data)
+    st.success('The predicted mark is {}'.format(result))
+
+
+
+
+
+
